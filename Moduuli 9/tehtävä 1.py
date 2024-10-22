23883960dd4136ebcdaa84f1395404e85bee2fd2
Ville Vartio <ville.vartio@metropolia.fi>
2024-10-22 11:53:41 +0300

    patch notes: added the completed task 9.1

--- Moduuli 9/tehtävä 1.py
+++ Moduuli 9/tehtävä 1.py
@@ -1,2 +1,12 @@
 class Auto:
-    pass
+    currentspeed = 0
+    topspeed = 0
+    distance_traveled = 0
+    def __init__(self, registry, topspeed):
+        self.registry = registry
+        self.topspeed = topspeed
+
+auto = Auto(input("reg"), input("speed"))
+
+print(f"The car has been registered as: {auto.registry}, with a top speed of {auto.topspeed} km/h.")
+print(f"Current speed: {auto.currentspeed}, distance traveled: {auto.distance_traveled}.")
